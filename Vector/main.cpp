5fe7f4f137103ce8ee6976ac37e9768b17f32b66
Alexander <aleksandr_chori@mail.ru>
2016-11-10 00:18:33 +1000

    Add files via upload
    Added source code

---
+++ Vector/main.cpp
@@ -0,0 +1,177 @@
+#define CATCH_CONFIG_MAIN
+#define _SCL_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include "include\catch.h"
+
+#include "vector.hpp"
+
+TEST_CASE("block1") {
+	class testClass1 {
+	public:
+		testClass1() : IsCreatedByDefaultConstructor(true) {};
+
+		bool IsCreatedByDefaultConstructor;
+	};
+
+	my :: vector<int> buff;
+	my :: vector<int> buff1(10);
+
+	SECTION("Default constructor") {
+		REQUIRE(buff.size() == 0);
+		REQUIRE(buff.capacity() == 0);
+	}
+
+	SECTION(".reserve()") {
+		buff.reserve(100);
+
+		REQUIRE(buff.capacity() == 100);
+
+		my :: vector<int> newBuff(15);
+
+		newBuff[4] = 17;
+		newBuff.reserve(5);
+
+		REQUIRE(newBuff.capacity() == 15);
+		REQUIRE(newBuff[4] == 17);
+
+		newBuff.reserve(50);
+
+		REQUIRE(newBuff[4] == 17);
+	}
+
+	SECTION("Default constructor of template class when initializing") {
+		my :: vector<testClass1> checkOnClass1(10);
+
+		REQUIRE(checkOnClass1.size() == 10);
+		REQUIRE(checkOnClass1.capacity() == 10);
+
+		bool trueForAll(true);
+
+		for (int i = 0; trueForAll && i < 10; i++) {
+			trueForAll = checkOnClass1[i].IsCreatedByDefaultConstructor;
+		}
+
+		REQUIRE(trueForAll);
+	}
+	SECTION("[] overload") {
+		my :: vector<testClass1> checkOnClass1(1);
+		REQUIRE(checkOnClass1[0].IsCreatedByDefaultConstructor); 
+
+		buff1[5] = 7;
+		REQUIRE(buff1[5] == 7);
+	}
+
+	SECTION("== & != overload") {
+		my::vector<char> str1(3);
+		my::vector<char> str2(3);
+
+		str1[0] = str2[0] = 'a';
+		str1[1] = str2[1] = 'b';
+		str1[2] = str2[2] = 'c';
+
+		REQUIRE(str1 == str2);
+
+		my::vector<char> str3(str2);
+
+		REQUIRE(str3 == str2);
+
+		str3[2] = 'd';
+
+		REQUIRE(str3 != str2);
+
+		str1.insert(str1.begin(), 'P');
+
+		REQUIRE(str1 != str2);
+	}
+
+	SECTION("= overload") {
+		my :: vector<char> str(5);
+		my :: vector<char> bigStr(100);
+		my :: vector<char> emptyStr;
+		int i = 0;
+		for (auto it = str.begin(); it != str.end(); it++) {
+			*it = 'A' + (i % 3);
+		}
+
+		bigStr = str;
+		REQUIRE(bigStr == str);
+
+		emptyStr = str;
+		REQUIRE(emptyStr == str);
+	}
+
+	SECTION("Copying constructor") {
+		my :: vector<int> buff2(buff1);
+
+		REQUIRE(buff1.size() == buff2.size());
+		REQUIRE(buff1.capacity() == buff2.capacity());
+
+		bool trueForAll(true);
+
+		for (unsigned i = 0; i < buff1.size() && trueForAll; i++) {
+			trueForAll = buff1[i] == buff2[i];
+		}
+
+		REQUIRE(trueForAll);
+	}
+
+	SECTION("Iterators") {
+		my :: vector<char> str(10);
+
+		for (int i = 0; i < 10; i++) {
+			str[i] = 'a' + i;
+		}
+
+		str.end() != str.begin();
+		(str.begin())++;
+		char c = *(++(str.begin()));
+		*(str.begin()) = 'd';
+		c = *(str.begin());
+
+		str.insert(++str.begin(), 'f');
+
+		std :: copy(str.begin(), str.end(), std :: ostream_iterator<char>(std :: cout, " "));
+	}
+
+	SECTION(".empty()") {
+		REQUIRE(buff.empty());
+		REQUIRE(!buff1.empty());
+	}
+
+	SECTION(".front() & .back()") {
+		my :: vector<bool> values(1);
+		values[0] = true;
+
+		REQUIRE(values.front());
+
+		values[0] = false;
+
+		REQUIRE(!values.back());
+	}
+
+	SECTION(".clear()") {
+		buff.clear();
+
+		REQUIRE(buff.size() == 0);
+		REQUIRE(buff.capacity() == 0);
+		REQUIRE(buff.empty());
+	}
+
+	SECTION(".push_back()") {
+		my :: vector<int> vec;
+
+		vec.push_back(12);
+		vec.push_back(24);
+		vec.push_back(50);
+
+		my :: vector<int> clone(3);
+
+		clone[0] = 12;
+		clone[1] = 24;
+		clone[2] = 50;
+
+		REQUIRE(vec == clone);
+	}
+}
---
+++ Vector/vector.hpp
@@ -0,0 +1,299 @@
+#pragma once
+
+#include <iterator>
+
+namespace my {
+	template <typename T>
+	class vector {
+	public:
+		vector() : _capacity(0), _size(0) {};
+		vector(vector<T>& src);
+		vector(int i);
+		~vector();
+
+		class Iterator : public std :: iterator<std :: input_iterator_tag, T>{
+		public:
+			friend class vector;
+
+			Iterator operator++();
+			Iterator operator--();
+			Iterator operator++(int);
+			Iterator operator--(int);
+			T& operator*();
+
+			bool operator!=(const Iterator& rhs);
+			bool isValid();
+		private:
+			T* data;
+		};
+
+		T& operator[](int i);
+		bool operator==(vector<T>& rhs);
+		bool operator!=(vector<T>& rhs);
+		vector<T> operator=(vector<T>& rhs);
+
+		Iterator begin();
+		Iterator end();
+		T& front();
+		T& back();
+		void clear();
+		void insert(typename vector<T> :: Iterator& iterator, const T& value);
+		void push_back(T value);
+		void reserve(const unsigned i);
+		bool empty();
+		unsigned capacity();
+		unsigned size();
+	private:
+		T* array;
+		unsigned _capacity;
+		unsigned _size;
+	};
+
+	template<typename T>
+	inline vector<T> :: vector(vector<T>& src) : _capacity(src.capacity()), _size(src.size())
+	{
+		array = new T[_capacity];
+
+#ifdef _SCL_SECURE_NO_WARNINGS
+		if (_capacity > 0) {
+			std :: copy(&(src[0]), &(src[_size]), array);
+		}
+#else
+		for (unsigned i = 0; i < _size; i++) {
+			array[i] = src[i];
+		}
+#endif
+	}
+
+	template<typename T>
+	inline vector<T> :: vector(int i)
+	{
+		_capacity = _size = i;
+		array = new T[i];
+	}
+
+	template<typename T>
+	inline vector<T> :: ~vector() 
+	{
+		delete array;
+	}
+
+	template<typename T>
+	inline typename vector<T> :: Iterator vector<T> :: begin()
+	{
+		Iterator iterator;
+		iterator.data = array;
+
+		return iterator;
+	}
+
+	template<typename T>
+	inline typename vector<T> :: Iterator vector<T> :: end()
+	{
+		Iterator iterator;
+		iterator.data = array + _size;
+
+		return iterator;
+	}
+
+	template<typename T>
+	inline typename T& vector<T> :: front()
+	{
+		return *(begin());
+	}
+
+	template<typename T>
+	inline typename T& vector<T> :: back()
+	{
+		return *(--end());
+	}
+
+	template<typename T>
+	inline void vector<T> :: clear()
+	{
+		_size = _capacity = 0;
+		delete array;
+	}
+
+	template<typename T>
+	inline T& vector<T> :: operator[](int i)
+	{
+		return array[i];
+	}
+
+	template<typename T>
+	inline bool vector<T> :: operator==(vector<T>& rhs)
+	{
+
+		if (_size != rhs.size()) {
+			return false;
+		}
+
+		bool areEqual(true);
+
+		for (unsigned i = 0; i < _size && areEqual; i++) {
+			areEqual = array[i] == rhs[i];
+		}
+
+		return areEqual;
+	}
+
+	template<typename T>
+	inline bool vector<T> :: operator!=(vector<T>& rhs)
+	{
+		return !(*this == rhs);
+	}
+
+	template<typename T>
+	inline vector<T> vector<T> :: operator=(vector<T>& rhs)
+	{
+		if (rhs.size() >= _capacity) {
+			T* buff = new T[_capacity = rhs.size() * 2];
+
+			int i = 0;
+			for (auto it = rhs.begin(); it != rhs.end(); it++, i++) {
+				buff[i] = *it;
+			}
+
+			delete array;
+			array = buff;
+		}
+		else {
+			int i = 0;
+			for (auto it = rhs.begin(); it != rhs.end(); it++, i++) {
+				array[i] = *it;
+			}
+		}
+
+		_size = rhs.size();
+		return *this;
+	}
+
+	template<typename T>
+	inline void vector<T> :: insert(typename vector<T> :: Iterator& iterator, const T& value)
+	{
+		T* buff;
+		if (_capacity <= ++_size) {
+			_capacity = _capacity == 0 ? 1 : _capacity * 2;
+		}
+		buff = new T[_capacity];
+
+		int i = 0;
+
+		for (auto it = begin(); it != iterator; it++, i++) {
+			buff[i] = *it;
+		}
+		int j = i++;
+		buff[j] = value;
+		if (iterator.isValid()) {
+			for (auto it = iterator; it != end(); it++, i++) {
+				buff[i] = *it;
+			}
+		}
+
+		delete array;
+		array = buff;
+
+		iterator.data = &(array[j]); //sickening crutch -- тошнотворный костыль
+	}
+
+	template<typename T>
+	inline void vector<T> :: push_back(T value)
+	{
+		insert(end(), value);
+	}
+
+	template<typename T>
+	inline void vector<T> :: reserve(const unsigned i)
+	{
+		if (i < _size) {
+			return;
+		}
+
+		T* buff = new T[i];
+
+#ifdef _SCL_SECURE_NO_WARNINGS
+		if (array) {
+			std :: copy(array, &(array[_size - 1]), buff);
+
+			delete array;
+		}
+		
+		array = buff;
+#else
+		delete array;
+
+		for (unsigned i = 0; i < _size; i++) {
+			buff[i] = array[i];
+		}
+#endif
+		_capacity = i;
+	}
+
+	template<typename T>
+	inline bool vector<T> :: empty()
+	{
+		return _size <= 0;
+	}
+
+	template<typename T>
+	inline unsigned vector<T> :: capacity()
+	{
+		return _capacity;
+	}
+
+	template<typename T>
+	inline unsigned vector<T> :: size()
+	{
+		return _size;
+	}
+
+	template<typename T>
+	inline typename vector<T> :: Iterator vector<T> :: Iterator :: operator++()
+	{
+		data++;
+		return *this;
+	}
+
+	template<typename T>
+	inline typename vector<T> :: Iterator vector<T> :: Iterator :: operator--()
+	{
+		data--;
+		return *this;
+	}
+
+	template<typename T>
+	inline typename vector<T> :: Iterator vector<T> :: Iterator :: operator++(int)
+	{
+		Iterator old(*this);
+		++(*this);
+		return old;
+	}
+
+	template<typename T>
+	inline typename vector<T> :: Iterator vector<T> :: Iterator :: operator--(int)
+	{
+		Iterator old(*this);
+		--(*this);
+		return old;
+	}
+
+	template<typename T>
+	inline T& vector<T> :: Iterator :: operator*()
+	{
+		return *data;
+	}
+
+	template<typename T>
+	inline bool vector<T> :: Iterator :: operator!=(const Iterator& rhs)
+	{
+		return data != rhs.data;
+	}
+
+	template<typename T>
+	inline bool vector<T> :: Iterator :: isValid()
+	{
+		return data != 0;
+	}
+
+}
