41af539e5bd6375c6a73b90fe5211b5de72034d7
Alexander <aleksandr_chori@mail.ru>
2016-11-18 03:42:06 +1000

    Update vector.hpp
    Added allocator

--- Vector/vector.hpp
+++ Vector/vector.hpp
@@ -4,18 +4,18 @@
 #include "Allocator.hpp"
 
 namespace my {
-	//TODO: Использоавть свой аллокатор
 	int calledDestructorForN = 0;
 
-	template <typename T>
+	template <typename T, typename alloc = Allocator<T>>
 	class vector {
 	public:
-		vector() : _capacity(0), _size(0), array(NULL) {};
+		vector() : _capacity(0), _size(0), array(NULL), allocator(new alloc) {};
 		vector(vector<T>& src);
-		vector(int i);
+		template <class... Args>
+			vector(int i, Args&& ...args);
 		~vector();
 
-		class Iterator : public std :: iterator<std :: input_iterator_tag, T>{
+		class Iterator : public std :: iterator<std :: random_access_iterator_tag, T>{
 		public:
 			friend class vector;
 
@@ -24,21 +24,24 @@ namespace my {
 			Iterator operator++(int);
 			Iterator operator--(int);
 			T& operator*();
+			bool operator<(Iterator&);
 
 			bool operator!=(const Iterator& rhs);
 			bool isValid();
 		private:
 			T* data;
 		};
-
+		
 		T& operator[](int i);
 		const T& operator[](int i) const;
 		bool operator==(vector<T>& rhs);
 		bool operator!=(vector<T>& rhs);
-		vector<T> operator=(vector<T>& rhs);
+		vector<T, alloc> operator=(vector<T>& rhs);
 
+		alloc* getAllocator();
 		Iterator begin();
 		Iterator end();
+		T& at(unsigned i);
 		T& front();
 		T& back();
 		void clear();
@@ -50,15 +53,15 @@ namespace my {
 		unsigned size();
 	private:
 		T* array;
-		T* array1;
+		alloc* allocator;
 		unsigned _capacity;
 		unsigned _size;
 	};
 
-	template<typename T>
-	inline vector<T> :: vector(vector<T>& src) : _capacity(src.capacity()), _size(src.size())
+	template<typename T, typename alloc>
+	inline vector<T, alloc> :: vector(vector<T>& src) : _capacity(src.capacity()), _size(src.size()), allocator(new alloc)
 	{
-		array = new T[_capacity];
+		array = allocator->allocate(_capacity);
 
 #ifdef _SCL_SECURE_NO_WARNINGS
 		if (_capacity > 0) {
@@ -71,21 +74,29 @@ namespace my {
 #endif
 	}
 
-	template<typename T>
-	inline vector<T> :: vector(int i)
+	template<typename T, typename alloc>
+	template<class... Args>
+	inline vector<T, alloc> :: vector(int i, Args&& ...args) : _capacity(i), _size(i), allocator(new alloc)
 	{
-		_capacity = _size = i;
-		array = new T[i];
+		array = allocator->allocate(i);
+		allocator->construct(array, std::forward<Args>(args)...);
 	}
 
-	template<typename T>
-	inline vector<T> :: ~vector() 
+	template<typename T, typename alloc>
+	inline vector<T, alloc> :: ~vector()
 	{
-		delete[] array;
+		allocator->destroy(array);
+		allocator->deallocate(array, _capacity);
 	}
 
-	template<typename T>
-	inline typename vector<T> :: Iterator vector<T> :: begin()
+	template<typename T, typename alloc>
+	inline alloc* vector<T, alloc> :: getAllocator()
+	{
+		return allocator;
+	}
+
+	template<typename T, typename alloc>
+	inline typename vector<T, alloc> :: Iterator vector<T, alloc> :: begin()
 	{
 		Iterator iterator;
 		iterator.data = array;
@@ -93,8 +104,8 @@ namespace my {
 		return iterator;
 	}
 
-	template<typename T>
-	inline typename vector<T> :: Iterator vector<T> :: end()
+	template<typename T, typename alloc>
+	inline typename vector<T, alloc> :: Iterator vector<T, alloc> :: end()
 	{
 		Iterator iterator;
 		iterator.data = array + _size;
@@ -102,39 +113,48 @@ namespace my {
 		return iterator;
 	}
 
-	template<typename T>
-	inline typename T& vector<T> :: front()
+	template<typename T, typename alloc>
+	inline T& vector<T, alloc> ::at(unsigned i)
+	{
+		if (i >= _size) {
+			throw("Wrong index");
+		}
+		return array[i];
+	}
+
+	template<typename T, typename alloc>
+	inline typename T& vector<T, alloc> :: front()
 	{
 		return *(begin());
 	}
 
-	template<typename T>
-	inline typename T& vector<T> :: back()
+	template<typename T, typename alloc>
+	inline typename T& vector<T, alloc> :: back()
 	{
 		return *(--end());
 	}
 
-	template<typename T>
-	inline void vector<T> :: clear()
+	template<typename T, typename alloc>
+	inline void vector<T, alloc> :: clear()
 	{
 		_size = _capacity = 0;
 		delete array;
 	}
 
-	template<typename T>
-	inline T& vector<T> :: operator[](int i)
+	template<typename T, typename alloc>
+	inline T& vector<T, alloc> :: operator[](int i)
 	{
 		return array[i];
 	}
 
-	template<typename T>
-	inline const T& vector<T> :: operator[](int i) const
+	template<typename T, typename alloc>
+	inline const T& vector<T, alloc> :: operator[](int i) const
 	{
 		return array[i];
 	}
 
-	template<typename T>
-	inline bool vector<T> :: operator==(vector<T>& rhs)
+	template<typename T, typename alloc>
+	inline bool vector<T, alloc> :: operator==(vector<T>& rhs)
 	{
 
 		if (_size != rhs.size()) {
@@ -150,24 +170,25 @@ namespace my {
 		return areEqual;
 	}
 
-	template<typename T>
-	inline bool vector<T> :: operator!=(vector<T>& rhs)
+	template<typename T, typename alloc>
+	inline bool vector<T, alloc> :: operator!=(vector<T>& rhs)
 	{
 		return !(*this == rhs);
 	}
 
-	template<typename T>
-	inline vector<T> vector<T> :: operator=(vector<T>& rhs)
+	template<typename T, typename alloc>
+	inline vector<T, alloc> vector<T, alloc> :: operator=(vector<T>& rhs)
 	{
+		unsigned deCapacity = _capacity;
 		if (rhs.size() >= _capacity) {
-			T* buff = new T[_capacity = rhs.size() * 2];
+			T* buff = allocator->allocate(_capacity = rhs.size() * 2);
 
 			int i = 0;
 			for (auto it = rhs.begin(); it != rhs.end(); it++, i++) {
 				buff[i] = *it;
 			}
 
-			delete array;
+			allocator->deallocate(array, deCapacity);
 			array = buff;
 		}
 		else {
@@ -181,14 +202,15 @@ namespace my {
 		return *this;
 	}
 
-	template<typename T>
-	inline void vector<T> :: insert(typename vector<T> :: Iterator& iterator, const T& value)
+	template<typename T, typename alloc>
+	inline void vector<T, alloc> :: insert(typename vector<T> :: Iterator& iterator, const T& value)
 	{
 		T* buff;
+		unsigned deCapacity = _capacity;
 		if (_capacity <= ++_size) {
 			_capacity = _capacity == 0 ? 1 : _capacity * 2;
 		}
-		buff = new T[_capacity];
+		buff = allocator->allocate(_capacity);
 
 		int i = 0;
 
@@ -203,37 +225,39 @@ namespace my {
 			}
 		}
 
-		delete array;
+		allocator->deallocate(array, deCapacity);
 		array = buff;
 
-		iterator.data = &(array[j]); //sickening crutch -- тошнотворный костыль
+		iterator.data = &(array[j]); //sickening crutch -- С‚РѕС€РЅРѕС‚РІРѕСЂРЅС‹Р№ РєРѕСЃС‚С‹Р»СЊ
 	}
 
-	template<typename T>
-	inline void vector<T> :: push_back(T value)
+	template<typename T, typename alloc>
+	inline void vector<T, alloc> :: push_back(T value)
 	{
 		insert(end(), value);
 	}
 
-	template<typename T>
-	inline void vector<T> :: reserve(const unsigned i)
+	template<typename T, typename alloc>
+	inline void vector<T, alloc> :: reserve(const unsigned i)
 	{
 		if (i < _size) {
 			return;
 		}
 
-		T* buff = new T[i];
+		T* buff = allocator->allocate(i);
 
 #ifdef _SCL_SECURE_NO_WARNINGS
 		if (array) {
 			std :: copy(array, &(array[_size - 1]), buff);
 
-			delete array;
+			allocator->destroy(array);
+			allocator->deallocate(array, _capacity);
 		}
 		
 		array = buff;
 #else
-		delete array;
+		allocator->destroy(array);
+		allocator->deallocate(array, _capacity);
 
 		for (unsigned i = 0; i < _size; i++) {
 			buff[i] = array[i];
@@ -242,68 +266,74 @@ namespace my {
 		_capacity = i;
 	}
 
-	template<typename T>
-	inline bool vector<T> :: empty()
+	template<typename T, typename alloc>
+	inline bool vector<T, alloc> :: empty()
 	{
 		return _size <= 0;
 	}
 
-	template<typename T>
-	inline unsigned vector<T> :: capacity()
+	template<typename T, typename alloc>
+	inline unsigned vector<T, alloc> :: capacity()
 	{
 		return _capacity;
 	}
 
-	template<typename T>
-	inline unsigned vector<T> :: size()
+	template<typename T, typename alloc>
+	inline unsigned vector<T, alloc> :: size()
 	{
 		return _size;
 	}
 
-	template<typename T>
-	inline typename vector<T> :: Iterator vector<T> :: Iterator :: operator++()
+	template<typename T, typename alloc>
+	inline typename vector<T, alloc> :: Iterator vector<T, alloc> :: Iterator :: operator++()
 	{
 		data++;
 		return *this;
 	}
 
-	template<typename T>
-	inline typename vector<T> :: Iterator vector<T> :: Iterator :: operator--()
+	template<typename T, typename alloc>
+	inline typename vector<T, alloc> :: Iterator vector<T, alloc> :: Iterator :: operator--()
 	{
 		data--;
 		return *this;
 	}
 
-	template<typename T>
-	inline typename vector<T> :: Iterator vector<T> :: Iterator :: operator++(int)
+	template<typename T, typename alloc>
+	inline typename vector<T, alloc> :: Iterator vector<T, alloc> :: Iterator :: operator++(int)
 	{
 		Iterator old(*this);
 		++(*this);
 		return old;
 	}
 
-	template<typename T>
-	inline typename vector<T> :: Iterator vector<T> :: Iterator :: operator--(int)
+	template<typename T, typename alloc>
+	inline typename vector<T, alloc> :: Iterator vector<T, alloc> :: Iterator :: operator--(int)
 	{
 		Iterator old(*this);
 		--(*this);
 		return old;
 	}
 
-	template<typename T>
-	inline T& vector<T> :: Iterator :: operator*()
+	template<typename T, typename alloc>
+	inline T& vector<T, alloc> :: Iterator :: operator*()
 	{
 		return *data;
 	}
 
-	template<typename T>
-	inline bool vector<T> :: Iterator :: operator!=(const Iterator& rhs)
+	template<typename T, typename alloc>
+	inline bool vector<T, alloc> :: Iterator :: operator<(Iterator& x)
+	{
+		return data < x.data;
+	}
+
+	template<typename T, typename alloc>
+	inline bool vector<T, alloc> :: Iterator :: operator!=(const Iterator& rhs)
 	{
 		return data != rhs.data;
 	}
 
-	template<typename T>
-	inline bool vector<T> :: Iterator :: isValid()
+	template<typename T, typename alloc>
+	inline bool vector<T, alloc> :: Iterator :: isValid()
 	{
 		return data != 0;
 	}
