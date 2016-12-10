ad7ee31f46c8bd01a6c1ac14b87f006023b79bdf
Alexander <aleksandr_chori@mail.ru>
2016-12-10 11:16:30 +1000

    Update Matrix.hpp
    Definition for nothread multiplication

--- includes/Matrix.hpp
+++ includes/Matrix.hpp
@@ -11,33 +11,35 @@
 
 class Matrix {
 public:
-	Matrix (unsigned n, unsigned m = 0, std :: string argv = "");
+	Matrix(unsigned n, unsigned m = 0, std::string argv = "");
 	Matrix() = default;
-    ~Matrix();
+	~Matrix();
 
-    std :: vector<double>&  operator[](int i);
-	std :: vector<double>   operator()(unsigned  i);
-    friend std :: istream&  operator>>(std :: istream& is, Matrix& x);
-    friend std :: ostream&  operator<<(std :: ostream& os, Matrix& x);
-	friend std :: ifstream& operator>>(std :: ifstream& is, Matrix& x);
+	std::vector<double>&  operator[](int i);
+	std::vector<double>   operator()(unsigned  i);
+	friend std::istream&  operator>>(std::istream& is, Matrix& x);
+	friend std::ostream&  operator<<(std::ostream& os, Matrix& x);
+	friend std::ifstream& operator>>(std::ifstream& is, Matrix& x);
 
 	Matrix					 operator*(Matrix& x);
-	std :: vector<double>	 operator*(std :: vector<double>& x);
+	std::vector<double>	 operator*(std::vector<double>& x);
 	Matrix					 operator*(double x);
 
-	std :: vector<double>	 quadratize();
+	std::vector<double>	 quadratize();
 	Matrix					 transpose();
-    unsigned int			 cols();
+	unsigned int			 cols();
 	unsigned int			 rows();
 
 	static unsigned	threads;
 protected:
-    std :: vector<std :: vector<double>> tab;
+	std::vector<std::vector<double>> tab;
 };
 
-std :: vector<double> solve(Matrix a);
+std::vector<double> solve(Matrix a);
 
-std :: vector<double> vsMul(std :: vector<double>& lhs, double rhs);
-double scalarMul(std :: vector<double>& lhs, std :: vector<double>& rhs);
+std::vector<double> vsMul(std::vector<double>& lhs, double rhs);
+double scalarMul(std::vector<double>& lhs, std::vector<double>& rhs);
+
+Matrix nothread(Matrix& lhs, Matrix& rhs);
 
 #endif //NM_DIVSUB_MATRIX_HPP
